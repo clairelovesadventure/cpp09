06a2bcafbf73cffdcb56bc0b98314592b710d90a
claire_loves_adventure <tanshuxin2000@gmail.com>
2026-07-06 16:21:10 +0200

    changed

--- ex01/RPN.hpp
+++ ex01/RPN.hpp
@@ -4,18 +4,18 @@
 #include <iostream>
 #include <string>
 #include <stack>
-#include <vector>
+#include <list> // 👈 将 <vector> 替换为 <list>
 
 class RPN {
     private:
-        // 显式指定底层为 std::vector，防止“白嫖”了默认的 deque
-        std::stack<int, std::vector<int> > _stack;
+        // 👈 显式指定底层为 std::list，既避开了默认的 deque，又把清白的 vector 留给了 ex02
+        std::stack<int, std::list<int> > _stack;
 
         // 私有辅助函数
         bool _isOperator(char c) const;
         bool _executeOperation(char op);
 
-        // 禁止外部使用拷贝和赋值（或者你也可以像 Ex00 一样完整实现它们）
+        // 禁止外部使用拷贝和赋值
         RPN(const RPN& src);
         RPN& operator=(const RPN& rhs);
 
